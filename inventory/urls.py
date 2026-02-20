3f952dcd8e8ae28323ca945419b2ac67b1943467
Creed <redota.jadecarlo.dll@gmail.com>
2026-02-20 10:02:04 +0800

    Fix API router urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from .views import ItemViewSet, AllocationViewSet
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
 router.register(r'allocations', AllocationViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),  # <- important
+]
